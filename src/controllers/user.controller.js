2082c52f9733faf6fcb0ea640d8c74db5a0b5645
letusDeliver <134798398+letusDeliver@users.noreply.github.com>
2025-12-09 07:58:06 +0530

    register user and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,20 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Kunal and code",
-  });
+  //get user details from front end
+  //validation - not empty
+  //check if user already exists: check with both email and username
+  //check for images, check for avatar
+  //upoload them to cloudinary, avatar check
+  //create user object - create entry in db
+  //remove password and refresh token field from response
+  //check for user creation
+  //return response or error
+
+  const {username, email, fullname, password} = req.body;
+  console.log(email, password);
+
+
 });
 
 export { registerUser };
